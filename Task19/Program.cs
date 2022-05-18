a83253c065763b741db59b3fb90853c4ad6b3abe
OstapchukSergey <sq31@mail.ru>
2022-05-18 22:57:43 +0300

    task19,21,23 ready

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,4 +6,26 @@
 12821 -> да
 */
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите пятизначное число - ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 9999 && Number < 100000)
+{
+    int a5 = Number % 10;
+    int a4 = (Number / 10) % 10;
+    int a2 = (Number / 1000) % 10;
+    int a1 = Number / 10000;
+
+    if (a1 == a5 && a2 == a4)
+    {
+        Console.Write($"Введенное число {Number} является палиндромом!");
+    }
+    else
+    {
+        Console.Write($"Введенное число {Number} не является палиндромом!");
+    }
+}
+else
+{
+    Console.Write($"Введенное число {Number} не является пятизначным!");
+}
